0412acac28ae493b90fd06762b9b9ea7c6fc110e
luckyilia <ilia.dev@yahoo.com>
2022-01-19 21:54:06 +0300

    created app 'content'

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,6 +79,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "djsubservice.users",
+    "djsubservice.content",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
