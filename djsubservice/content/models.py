297c07f41f55844fc508fff3b1e198cbc3331b5d
luckyilia <ilia.dev@yahoo.com>
2022-01-19 22:02:32 +0300

    added Video model in 'content' app

--- djsubservice/content/models.py
+++ djsubservice/content/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(model.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    thumbnail = models.ImageField(upload_to="thumbnails/")
+
+    def __str__(self):
+        return self.name
+
+class Video(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='videos')
+    vimeo_id = models.CharField(max_length=50)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
